d83a0b032a3d5ed9c515de4e99b4de35e6c14a73
asuravadha <rr.easwar@gmail.com>
2021-10-04 11:55:42 +0530

    loops and control statemnets

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,170 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
+                int a,b;
         Scanner input=new Scanner(System.in);
+/*
+infinite loop in for
+for(;;){
+
+}
+in while
+while(){
+
+}
 
 
-        System.out.println("Enter your name");
-        String name=input.next();
+*/
+//        while(true){
+//            System.out.println("enter a,b values");
+//            a=input.nextInt();
+//            b=input.nextInt();
+//            System.out.println("Selct your option *,-,*,/,%");
+//            String option=input.next();
+//            if(option.equals("+")){
+//                System.out.println("a+b is"+(a+b));
+//            }else if(option.equals("-")){
+//                System.out.println("a-b is"+(a-b));
+//            }
+//            else if(option.equals("*")){
+//                System.out.println("a*b is"+(a*b));
+//            }
+//            else if(option.equals("/")){
+//                System.out.println("a/b is"+(a/b));
+//            }
+//            else if(option.equals("%")){
+//                System.out.println("a-b is"+(a%b));
+//            }
+//            else{
+//                System.out.println("invalid input");
+//            }
+//            System.out.println("Enter q to quit or anyother to proceed");
+//            option=input.next();
+//            if(option.equals("q")){
+//System.out.println("end of code");
+//                break;
+//            }
+//
+//        }
 
-        System.out.println("enter your role");
-        String role=input.next();
 
-        System.out.println("give your experience details");
-        
-        int experience=input.nextInt();
-        System.out.println(name + " "+ role + "  "+experience);
+
+
+
+
+
+
+
+
+
+
+
+
+
+//        int i=1;
+//        while(i<=5){
+//            System.out.println(i);
+//            i=i+2;
+//        }
+//        System.out.println(i);
+
+
+
+
+//        int i;
+//        for(i=1;i<=5;i=i+2){
+//            System.out.println(i);
+//        }
+//        System.out.println(i);
+
+        //        int a=13,b=2;
+//
+//        System.out.println("Selct your option *,-,*,/,%");
+//        Scanner input=new Scanner(System.in);
+//        String option=input.next();
+//        if(option.equals("+")){
+//            System.out.println("a+b is"+(a+b));
+//        }else if(option.equals("-")){
+//            System.out.println("a-b is"+(a-b));
+//        }
+//        else if(option.equals("*")){
+//            System.out.println("a*b is"+(a*b));
+//        }
+//        else if(option.equals("/")){
+//            System.out.println("a/b is"+(a/b));
+//        }
+//        else if(option.equals("%")){
+//            System.out.println("a-b is"+(a%b));
+//        }
+//        else{
+//            System.out.println("invalid input");
+//        }
+
+
+
+
+
+
+
+
+
+
+
+//               String dusername="root",dpassword="toor";
+//
+//        String username,password;
+//
+//        Scanner input=new Scanner(System.in);
+//
+//        System.out.println("Enter username:");
+//        username=input.next();
+//
+//
+//
+//        if(username.equals(dusername)){
+//            System.out.println("Enter password:");
+//            password=input.next();
+//            if(password.equals(dpassword)){
+//                System.out.println("login successful");
+//            }
+//            else{
+//                System.out.println("wrong password");
+//            }
+//        }
+//        else{
+//            System.out.println("user not found");
+//        }
+
+
+
+
+
+
+
+
+
+
+//         System.out.println("Enter your password");
+//         password=input.next();
+//
+//         if(password.length()>=8){
+//             System.out.println("password saved ");
+//         }
+//         else{
+//             System.out.println("password should have atleast 8 characters ");
+//         }
+
+
+
+//        System.out.println("Enter your name");
+//        String name=input.next();
+//
+//        System.out.println("enter your role");
+//        String role=input.next();
+//
+//        System.out.println("give your experience details");
+//
+//        int experience=input.nextInt();
+//        System.out.println(name + " "+ role + "  "+experience);
     }
 }
