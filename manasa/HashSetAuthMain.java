c246b805fcc7528a9f3d4a189c4db4144ad46cba
manasa126 <92017002+manasa126@users.noreply.github.com>
2021-10-12 09:27:09 +0530

    Create HashSetAuthMain.java

---
+++ manasa/HashSetAuthMain.java
@@ -0,0 +1,119 @@
+ackage assignments;
+import java.util.Scanner;
+import java.util.Iterator;
+import java.util.Scanner;
+public class HashSet {
+    java.util.HashMap<String,String> users=new java.util.HashMap<>();
+    java.util.HashMap<String,String> data=new java.util.HashMap<>();
+    Scanner sc=new Scanner(System.in);
+
+    public void signup()
+    {
+
+        System.out.println("enter username");
+        String name=sc.next();
+        System.out.println("enter password");
+        String password=sc.next();
+        users.put(name,password);
+        System.out.println("enter the data");
+        String words=sc.next();
+        data.put(name,words);
+
+
+
+    }
+
+    public void display()
+    {
+        System.out.print(users);
+    }
+
+
+    public void login()
+    {
+        System.out.println("enter the username");
+        String name=sc.next();
+
+
+        if(users.containsKey(name))
+        {
+            System.out.println("user is present");
+            System.out.println("enter the password");
+            String password=sc.next();
+            if(users.containsValue(password))
+            {
+                System.out.println("password is sucessfull");
+                if(data.containsKey(name))
+                {
+                    System.out.println("username: "+name+" "+"data "+data.values());
+                }
+
+
+            }
+            else
+            {
+                System.out.println("password is incorrect");
+            }
+
+        }
+        else {
+            System.out.println("user is not present");
+        }
+
+
+
+
+    }
+}
+//Main
+
+package assignments;
+import java.util.Scanner;
+import java.util.HashSet;
+public class HashSetAuthMain {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        assignments.HashSet h1=new assignments.HashSet();
+        while(true)
+        {
+            System.out.println("enter s for signup and l for login and d to display all the users present signed up");
+            String enter=sc.next();
+            switch (enter)
+            {
+                case "s":
+                    System.out.println("signup");
+                    h1.signup();
+                    break;
+
+
+
+                case "l":
+                    System.out.println("Login");
+                    h1.login();
+                    break;
+                case "d":
+                    System.out.println("Users Signed Up");
+                    h1.display();
+                    break;
+            }
+
+
+
+
+            System.out.println("enter q to quit and any other to continue");
+            String option=sc.next();
+            if(option.equals("q"))
+            {
+                break;
+            }
+
+
+
+        }
+
+    }
+}
+
+
+
